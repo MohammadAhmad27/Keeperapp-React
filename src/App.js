8a1b691ed1fca7f003352bcffc8768a06dd17d43
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-02-29 18:25:26 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Note from './components/Note';
+import CreateArea from './components/CreateArea';
+
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    console.log("Delete is triggered")
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+
+  return (
+    <div className="App">
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
