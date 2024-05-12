c9a3f74e831dae8f51226c150d7db3fd7aa901f7
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-05-13 02:26:20 +0500

    Added useRef & useEffect to focus on title input

--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function CreateArea(props) {
 
-  const [note, setNote] = useState({
-    title: "",
-    content: ""
+  const [note, setNote] = useState(() => {
+    // Check if there are notes stored in localStorage
+    const storedNote = localStorage.getItem("note");
+    return storedNote ? JSON.parse(storedNote) : { title: "", content: "" };
   });
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    console.log("Storing note in localStorage:", note);
+    localStorage.setItem("note", JSON.stringify(note));
+  }, [note]);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [note.title]);
+
+  useEffect(() => {
+    localStorage.setItem("note", JSON.stringify(note));
+  }, [note]);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -19,20 +35,17 @@ function CreateArea(props) {
   }
 
   function submitNote(event) {
-    props.onAdd(note);
-    setNote({
-      title: "",
-      content: ""
-    })
     event.preventDefault();
+    props.onAdd(note);
+    setNote({ title: "", content: "" });
   }
 
   return (
     <div>
-      <form  className="create-note">
-        <input name="title" value={note.title} onChange={handleChange} placeholder="Title" />
+      <form className="create-note">
+        <input name="title" ref={inputRef} value={note.title} onChange={handleChange} placeholder="Title" />
         <textarea name="content" value={note.content} onChange={handleChange} placeholder="Take a note..." rows="3" />
-        <button onClick={submitNote}>Add</button>
+        <button disabled={note.title.length < 3 || note.content.length < 3} onClick={submitNote}>Add</button>
       </form>
     </div>
   );
